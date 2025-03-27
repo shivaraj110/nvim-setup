1e120e100af5d9cd1bd52013d1dae259e365afc1
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-03-27 14:58:27 +0530

    added discord presence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,36 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  'andweeb/presence.nvim',
+  lazy = false,
+  config = function()
+    require('presence').setup {
+      -- General options
+      auto_update = true,
+      neovim_image_text = 'The One True Text Editor',
+      main_image = 'neovim',
+      client_id = '793271441293967371',
+      log_level = nil,
+      debounce_timeout = 10,
+      enable_line_number = false,
+      blacklist = {
+        'NvimTree',
+        'neo-tree',
+        'dashboard',
+        'Telescope',
+        'lazy',
+        'mason',
+      },
+      buttons = true,
+      file_assets = {},
+      show_time = true,
+
+      -- Rich Presence text options
+      editing_text = 'Editing %s',
+      file_explorer_text = 'Browsing %s',
+      git_commit_text = 'Committing changes',
+      plugin_manager_text = 'Managing plugins',
+      reading_text = 'Reading %s',
+      workspace_text = 'Working on %s',
+      line_number_text = 'Line %s out of %s',
+    }
+  end,
+}
